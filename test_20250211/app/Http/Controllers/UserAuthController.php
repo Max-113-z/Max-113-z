122298a244d556c539b258a23aadc451486a0030
Max-113-z <a7830079@gmail.com>
2025-02-13 13:46:03 +0800

    暱稱不得為空

--- test_20250211/app/Http/Controllers/UserAuthController.php
+++ test_20250211/app/Http/Controllers/UserAuthController.php
@@ -9,12 +9,24 @@ class UserAuthController extends Controller
  
 
     public function SignUpPage()
-    {   
+    {
         $binding = [
             'title' => '註冊',
-            'note' =>'使用者註冊頁面'
+            'note' => '使用者註冊頁面'
         ];
         return view('auth.signup', $binding);
+    }
+
+    public function SignUpProcess()
+    {
+        $input = request()->all();
+        print_r($input);
+
+        if ($input['nickname'] == '') {
+            print('暱稱不得為空');
+            return redirect('/user/auth/signup')
+                ->withErrors(['暱稱不得為空']);
+        }
     }
 
 };
